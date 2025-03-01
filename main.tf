13fddb37701828e577c5f59bc3d31782b727e5a3
gio-cloud7 <136278206+gio-cloud7@users.noreply.github.com>
2025-03-01 08:49:31 -0500

    Update main.tf
    changed app name to "template-app"

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "serverless-streamlit-app" {
   source          = "aws-ia/serverless-streamlit-app/aws"
-  app_name        = "rsl-app"
+  app_name        = "template-app"
   app_version     = "v1.1.0" 
   path_to_app_dir = "./app" # Replace with path to your app
   
