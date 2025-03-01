834bef9d6ce3cb0b60dec9c2125188c1f313b8b1
gio-cloud7 <136278206+gio-cloud7@users.noreply.github.com>
2025-03-01 07:39:37 -0500

    Create main.tf
    Streamlit Module main.tf Template

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "serverless-streamlit-app" {
+  source          = "aws-ia/serverless-streamlit-app/aws"
+  app_name        = "rsl-app"
+  app_version     = "v1.1.0" 
+  path_to_app_dir = "./app" # Replace with path to your app
+  
+  
+}
